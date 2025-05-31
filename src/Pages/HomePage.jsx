b223932c807f28dcd49d686edb43704b7e89d13f
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-31 14:36:34 +0600

    Refactor HomePage styles and improve layout for better responsiveness

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -7,15 +7,17 @@ const HomePage = () => {
   return (
     <main className="min-h-screen bg-white flex flex-col items-center justify-center px-4 py-12">
       <div className="w-full mx-auto text-center">
-        <h1 className="text-[#1e2d5e] text-4xl md:text-5xl lg:text-6xl font-bold mb-6">
+        <div className='max-w-2xl mx-auto mb-8'>
+          <h1 className="text-primary text-2xl md:text-5xl lg:text-6xl font-bold mb-6">
           The Digital Home for Every Bowler
         </h1>
 
-        <p className="text-[#5d5d5d] text-lg max-w-3xl mx-auto mb-12">
+        <p className="text-secondary text-balance text-sm md:text-base lg:text-lg max-w-3xl mx-auto mb-12">
           Here's a clear separation of Premium vs Free Features for the BowlersNetwork platform, categorized by major
           functionality. This layout ensures your Super Admin can manage feature visibility and monetization easily via
           the admin pane
         </p>
+        </div>
 
         <div className="relative w-full overflow-hidden mb-12">
           <Slider/>
@@ -23,14 +25,14 @@ const HomePage = () => {
 
         <Link
           to="/get-started"
-          className="inline-block bg-[#8bc342] text-white font-medium py-4 px-8 rounded-md text-lg w-full max-w-md mb-6 hover:bg-opacity-90 transition-all"
+          className="inline-block bg-button-bg text-button-text font-medium py-3 md:py-4 px-8 mx-1 rounded-md text-sm md:text-base lg:text-lg w-full max-w-md mb-6 hover:bg-opacity-90 transition-all"
         >
           Get Started
         </Link>
 
-        <p className="text-[#5d5d5d]">
+        <p className="text-[#5d5d5d] text-sm md:text-base lg:text-lg">
           Already have an account?
-          <Link to="/signin" className="text-[#1e2d5e] font-medium ml-1 hover:underline">
+          <Link to="/signin" className="text-primary font-medium ml-1 hover:underline">
             Sign In
           </Link>
         </p>
